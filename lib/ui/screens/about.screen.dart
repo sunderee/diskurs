f6c602e57d5813ad5c70cd1223f6089ca2d6ae21
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-05-06 13:49:08 +0200

    Design about screen

--- lib/ui/screens/about.screen.dart
+++ lib/ui/screens/about.screen.dart
@@ -1,3 +1,5 @@
+import 'package:diskurs/utils/about_page.util.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -15,6 +17,302 @@ class _AboutScreenState extends State<AboutScreen> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Info'),
+      ),
+      body: SafeArea(
+        minimum: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            Text(
+              'SL',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: AboutPageUtil.ABOUT_SLOVENIAN[0],
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                    fontSize: 16.0,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SLOVENIAN[1]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SLOVENIAN[2],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SLOVENIAN[3]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SLOVENIAN[4],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SLOVENIAN[5]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SLOVENIAN[6],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SLOVENIAN[7]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SLOVENIAN[8],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SLOVENIAN[9]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SLOVENIAN[10],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SLOVENIAN[11]),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            Text(
+              'HR',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: AboutPageUtil.ABOUT_CROATIAN[0],
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                    fontSize: 16.0,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_CROATIAN[1]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_CROATIAN[2],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_CROATIAN[3]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_CROATIAN[4],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_CROATIAN[5]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_CROATIAN[6],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_CROATIAN[7]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_CROATIAN[8],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_CROATIAN[9]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_CROATIAN[10],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_CROATIAN[11]),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            Text(
+              'SR',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: AboutPageUtil.ABOUT_SERBIAN[0],
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                    fontSize: 16.0,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SERBIAN[1]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SERBIAN[2],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SERBIAN[3]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SERBIAN[4],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SERBIAN[5]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SERBIAN[6],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SERBIAN[7]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SERBIAN[8],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SERBIAN[9]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_SERBIAN[10],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_SERBIAN[11]),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 32.0,
+            ),
+            Text(
+              'EN',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                top: 16.0,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: AboutPageUtil.ABOUT_ENGLISH[0],
+                  style: TextStyle(
+                    color: Colors.grey[900],
+                    fontSize: 16.0,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_ENGLISH[1]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_ENGLISH[2],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_ENGLISH[3]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_ENGLISH[4],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(
+                      text: '\n\n',
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_ENGLISH[5]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_ENGLISH[6],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_ENGLISH[7]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_ENGLISH[8],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_ENGLISH[9]),
+                    TextSpan(
+                      text: AboutPageUtil.ABOUT_ENGLISH[10],
+                      style: TextStyle(
+                        color: Colors.lightBlue,
+                      ),
+                      recognizer: TapGestureRecognizer()..onTap = () {},
+                    ),
+                    TextSpan(text: AboutPageUtil.ABOUT_ENGLISH[11]),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
