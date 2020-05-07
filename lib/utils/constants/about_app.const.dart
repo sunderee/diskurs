b2546b06b8e072b1b86a3d0110b366a7ee856009
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-05-07 09:38:04 +0200

    Translate about page to multiple languages

--- lib/utils/constants/about_app.const.dart
+++ lib/utils/constants/about_app.const.dart
@@ -1,16 +1,46 @@
 const ABOUT_APP_SLOVENIAN = AboutAppModel(
   'O aplikaciji',
-  [],
+  [
+    'Diskurs je cross-platform mobilna aplikacija za iskanje sinonimov, s katero lahko poizvedujete korpuse za vzorce besedne rabe. Podpira tri glavne balkanske jezike: slovenski, hrvaški in srbski.',
+    'Aplikacija je razvita s pomočjo API-jev, ki jih je razvil kontekst.io.',
+    'Kontekst.io je iskalnik sorodnih besed in stavkov, ki se samodejno ustvarijo iz več besedilnih korpusov sodobne slovenščine in se kasneje prevedejo v hrvaški in srbski jezik.',
+    'Slovenski iskalnik temelji na modelu word2vec, ki vsaki besedi / frazi dodeli položaj v latentnem prostoru, tako da so besede, ki se uporabljajo v podobnih kontekstih, blizu tudi v latentnem prostoru.',
+    'Osnovni model, ki ga uporablja kontekst.io, vsebuje približno 600.000 besed in stavkov.',
+    'Diskurs prevzame idejo, ki jo je zastavil kontekst.io in jo manifestira v obliki cross-platform mobilne aplikacije. Zanaša se na API-je, ki jih ponuja kontekst.io.',
+    'Na območju Balkana nam primanjkuje ti. tezavra v obliki aplikacije. Čeprav so države močno napredovale v smeri digitalizacije slovarjev, še vedno nimamo primera, ki bi bil tako močan kot tezavri za večje jezike, na primer angleščina. Če to nameravamo spremeniti, je potrebno skupno prizadevanje.',
+    'Diskurs je morda prvi združeni tezaver za tri glavne jezike na območju Balkana: slovenski, hrvaški in srbski. V povezavi s primeri besed uporabe, aplikacija ponuja vseprisotni medij za študente in strokovnjake, ki gledajo na sorodne stavke bodisi, da bi obogatili svoje pisanje ali sklepali o pomenu besede s preverjanjem podobnih besednih zvez.',
+    'Aplikacijo je razvil Peter Aleksander Bizjak. Posebna zahvala gre Mateju Aleksovu za oblikovanje in logotipe ter Marku Plahuti iz podjetja Virostatiq, ki stoji za kontekst.io, za zagotavljanje dostopa do API-jev.',
+  ],
 );
 
 const ABOUT_APP_CROATIAN = AboutAppModel(
   'O aplikaciji',
-  [],
+  [
+    'Diskurs je cross-platform mobilna tezaurus aplikacija za tezaurus s mogućnošću upita korporacija za primjere upotrebe riječi u rečenici. Podržava tri glavna balkanska jezika: slovenski, hrvatski i srpski.',
+    'Aplikacija je razvijena uz pomoć API-ja koji napajaju kontekst.io.',
+    'Kontekst.io je tražilica za srodne riječi i izraze, koje se automatski generiraju iz nekoliko tekstualnih korpusa modernog slovenskog jezika, a kasnije prevode na hrvatski i srpski jezik.',
+    'Slovenska tražilica temelji se na modelu word2vec koji svakoj riječi / frazi dodjeljuje položaj u latentnom prostoru tako da riječi korištene u sličnom kontekstu također budu blizu latentnog prostora.',
+    'Bazni model koji koristi kontekst.io sadrži oko 600.000 riječi i izraza.',
+    'Diskurs uzima ideju koju je postavio kontekst.io i utjelovljuje je u obliku cross-platformske mobilne aplikacije. Oslanja se na API-jeve koje pruža kontekst.io.',
+    'U balkanskoj regiji nam nedostaje tezaurus aplikacija. Iako su države ostvarile solidan napredak u smjeru digitalizacije rječnika, tek treba vidjeti primjer toliko moćan kao tezauri iz glavnih jezika kao što je engleski. Ako to namjeravamo promijeniti, potreban je zajednički napor.',
+    'Diskurs je možda prvi ujedinjeni tezaur za tri glavna jezika na području Balkana: slovenski, hrvatski i srpski. Zajedno s primjerima riječi upotrebe, aplikacija nudi sveprisutni medij za studente i stručnjake koji gledaju na slične izraze ili kako bi obogatili svoje pisanje ili zaključili značenje riječi provjeravanjem sličnih izraza.',
+    'Aplikaciju je razvio Peter Aleksander Bizjak. Posebna zahvala ide Mateju Aleksovu za pružanje dizajna i logotipa, kao i Marku Plahuti iz tvrtke Virostatiq, koja stoji iza kontekst.io, za pružanje pristupa API-jevima.',
+  ],
 );
 
 const ABOUT_APP_SERBIAN = AboutAppModel(
   'O aplikaciji',
-  [],
+  [
+    'Diskurs je mobilna tezaurus aplikacija na više platformi sa mogućnošću upita korporacija za primere upotrebe reči u rečenici. Podržava tri glavna balkanska jezika: slovenački, hrvatski i srpski.',
+    'Aplikacija je razvijena uz pomoć API-ja koji napajaju kontekst.io.',
+    'Kontekst.io je pretraživač srodnih reči i fraza, koje se automatski generišu iz više tekstnih korpusa modernog slovenačkog jezika, a kasnije prevode na hrvatski i srpski jezik.',
+    'Slovenački pretraživač zasnovan je na word2vec modelu koji svakoj reči / frazi dodeljuje poziciju u latentnom prostoru tako da se reči koje se koriste u sličnom kontekstu takođe nalaze blizu latentnog prostora.',
+    'Osnovni model koji koristi kontekst.io sadrži oko 600.000 reči i izraza.',
+    'Diskurs uzima ideju koju je postavio kontekst.io i otelovljuje je u obliku cross-platformske mobilne aplikacije. Oslanja se na API-jeve koje pruža kontekst.io.',
+    'U regionu Balkana nam nedostaje tezaurus aplikacija. Iako su države ostvarile solidan napredak u smeru digitalizacije rečnika, tek treba da vidimo primer toliko moćan kao tezaurus iz glavnih jezika kao što je engleski. Ako to nameravamo promeniti, potreban je zajednički napor.',
+    'Govoreći o tome, posmatramo možda i prvi ujedinjeni tezaur za tri glavna jezika na području Balkana: slovenački, hrvatski i srpski. Uporedo sa primerima upotrebe reči, aplikacija pruža sveprisutni medij za studente i stručnjake koji gledaju na povezane fraze bilo da obogate svoje pisanje ili da zaključe značenje reči putem provere sličnih fraza.',
+    'Aplikaciju je razvio Peter Aleksander Bizjak. Posebna zahvala ide Mateju Aleksovu za pružanje dizajna i logotipa, kao i Marku Plahuti iz kompanije Virostatiq, koja stoji iza kontekst.io, za pružanje pristupa API-jevima.',
+  ],
 );
 
 const ABOUT_APP_ENGLISH = AboutAppModel(
